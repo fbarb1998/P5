74a581c5b821657d2751d69da55924bde931f850
Barb <128657422+fbarb1998@users.noreply.github.com>
2024-06-01 22:32:57 -0400

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,14 @@
 const itemSection = document.getElementById("items");
 
+const baseUrl = 'http://localhost:3000/api';
+
+const jsonOrThrowIfError = async response => {
+  if(!response.ok) throw new Error((await response.json()).message);
+  return response.json();
+};
+
+export const fetchData = async ({ endpoint, init }) => jsonOrThrowIfError(await fetch(`${baseUrl + endpoint}`, init));
+
 fetch('http://localhost:3000/api/products')
     .then(data => {
         return data.json();
